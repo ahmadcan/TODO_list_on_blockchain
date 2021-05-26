9c87e866060a039d14774cef34452ecf13e588fb
ahmad <a.ahmad@outlook.de>
2021-05-27 00:41:25 +0200

    smart contract init commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.7.0;
+
+contract TodoList {
+
+    struct TodoItem {
+        string content;
+        bool checked;
+    }
+
+    mapping(address => TodoItem[]) todoList;
+
+    function getListForSender()
+    public
+    view
+    returns (TodoItem[] memory todos)
+    {
+        return todoList[msg.sender];
+    }
+
+    function addTodo(string memory _content) public {
+        todoList[msg.sender].push(TodoItem(_content, false));
+    }
+
+    function toggleTodo(uint _itemId) public {
+        TodoItem[] memory _todos = todoList[msg.sender];
+        require(_todos.length >= (_itemId - 1), "item doesn't exist");
+        todoList[msg.sender][_itemId].checked = !_todos[_itemId].checked;
+    }
+
+}
